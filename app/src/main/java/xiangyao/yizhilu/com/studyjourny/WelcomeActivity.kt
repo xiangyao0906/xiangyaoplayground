5745a20172e8bf361f41f03160ed4243bae7c235
xiangyao <sodagreen1994@163.com>
2017-09-06 18:09:14 +0800

    添加 pathview

--- app/src/main/java/xiangyao/yizhilu/com/studyjourny/WelcomeActivity.kt
+++ app/src/main/java/xiangyao/yizhilu/com/studyjourny/WelcomeActivity.kt
@@ -1,16 +1,39 @@
 package xiangyao.yizhilu.com.studyjourny
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.animation.AccelerateDecelerateInterpolator
 import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
+
+    /*
+    *   引用自  https://github.com/geftimov
+    * **/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
-        guideImage!!.setOnClickListener { startActivity(Intent(this,MainActivity::class.java)) }
+//        guideImage!!.setOnClickListener { startActivity(Intent(this,MainActivity::class.java)) }
+
+//        val pathView = findViewById<PathView>(R.id.pathView)
+
     }
+
+    override fun onResume() {
+        super.onResume()
+
+        pathView.pathAnimator
+                .delay(100)
+                .duration(1500)
+                .interpolator(AccelerateDecelerateInterpolator())
+                .listenerEnd {
+                    startActivity(Intent(this,MainActivity::class.java))
+                    finish()
+                }
+                .start()
+    }
+
 }
